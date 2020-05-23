afa0339f25e127d903f4921fd6c7f89c19f0359c
glend <glend@DESKTOP-GTH9QGR>
2020-05-23 02:13:27 -0600

    labo 6

---
+++ src/main/java/com/example/demo/EstudianteService.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EstudianteService implements serviceEstudiante {
+	@Autowired
+	private EstudianteDaoImp dao = new EstudianteDaoImp();
+
+	@Override
+	public List<Estudiante> getEstudiantes() {
+		return dao.findAll();
+	}
+	@Transactional
+	@Override
+	public boolean insertEstudiante(Estudiante est) {
+		
+		return dao.insertEstudent(est);
+	}
+	
+	@Transactional
+	@Override
+	public boolean deleteEstudiante(int id) {
+		
+		return dao.deleteEstudiante(id);
+	}
+
+}
---
+++ src/main/java/com/example/demo/serviceEstudiante.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import java.util.List;
+
+public interface serviceEstudiante {
+	public List<Estudiante> getEstudiantes();
+	
+	public boolean insertEstudiante(Estudiante est);
+	
+	public boolean deleteEstudiante(int id);
+
+}
